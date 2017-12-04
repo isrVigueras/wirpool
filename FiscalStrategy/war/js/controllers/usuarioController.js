44f84d3f44ddd96584d452881815bbb7398fd83f
chavo <chavo@tikal>
2017-12-04 16:52:41 -0600

    Validacion de Contraseña y correo

--- FiscalStrategy/war/js/controllers/usuarioController.js
+++ FiscalStrategy/war/js/controllers/usuarioController.js
@@ -60,23 +60,53 @@ app.service("perfilservice",['$http', '$q', function($http, $q){
 }]);
 app.controller("userController",['$scope','$window', '$location', '$cookieStore','usuarioservice', function($scope, $window, $location, $cookieStore, usuarioservice){
 	
+	$scope.validate = function() {
+		  if ($scope.usuario.email != $scope.usuario.emailconfirm) {
+		    $scope.IsMatch=true;
+		    return false;
+		  }
+		  $scope.IsMatch=false;
+}
 
-	  
-	$scope.enviarFormulario = function() {
-		usuarioservice
-				.crearUsuario($scope.usuario)
-				.then(
-						function(data) {
-							alert("Usuario creado correctamente");
-							$location
-									.path("/modificarusuarios");
-						});
+$scope.validatePass = function() {
+	if($scope.usuario.pass != $scope.usuario.passconfirm){
+		$scope.IsMatchP=true;
+		return false;
 	}
+	$scope.IsMatchP=false;
+}		
+
+$scope.acceptSubmit = function() {
+	if($scope.form.email.$valid && $scope.form.emailconfirm.$valid && !$scope.IsMatch && $scope.form.pass.$valid && $scope.form.passconfirm.$valid && !$scope.IsMatchP){
+		return true;
+	}
+	return false;
 	
-	
+}
+$scope.EnviarFormulario = function() {
+	//console.log($scope.form.pass.$valid);
+	$scope.validate();
+	if(($scope.usuario.email != $scope.usuario.emailconfirm) || ($scope.usuario.pass != $scope.usuario.passconfirm)){
+		alert("Email o contraseña no coinciden")
+		//$window.location.reload();
+	}else{
+		if($scope.form.pass.$valid){
+			usuarioservice
+			.crearUsuario($scope.usuario)
+			.then(
+					function(data) {
+						alert("Usuario creado correctamente");
+						$location
+								.path("/modificarusuarios");
+					})}else{
+						alert("Contraseña no valida, intente de nuevo");
+					}
+}}
+} ]);
+
 //	$scope.cargarPagina(1);
 	
-}]);
+
 app.controller("controladorListaUsuarios", [ '$scope', 'usuarioservice',
 	'perfilservice', '$location', '$window',
 	function($scope, usuarioservice, perfilservice, $location, $window) {
@@ -102,5 +132,7 @@ app.controller("controladorListaUsuarios", [ '$scope', 'usuarioservice',
 				$window.location.reload();
 			})
 		}
-	} ]);
+		
+				
+			} ]);
 
