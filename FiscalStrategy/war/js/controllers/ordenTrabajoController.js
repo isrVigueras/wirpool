16988ed47718f89c1adad110fd664991ebc1308e
chavo <chavo@tikal>
2017-12-20 14:47:27 -0600

    Ots,Perfil

---
+++ FiscalStrategy/war/js/controllers/ordenTrabajoController.js
@@ -0,0 +1,37 @@
+app.service("ordenTrabajoservice",['$http', '$q', function($http, $q){
+	this.data=function(data){
+		var dataClient={};
+	};
+	this.loadCliente = function(page) {
+		var d = $q.defer();
+	
+		$http.get("").then(
+			function(response) {
+				d.resolve(response.data);
+			});
+		return d.promise;
+	}
+	this.loadOT = function(id) {
+		var d = $q.defer();
+	
+		$http.get("/ots/find/"+id).then(
+			function(response) {
+				d.resolve(response.data);
+			});
+		return d.promise;
+	}
+	
+}]);
+
+app.controller("ordenTrabajoController",['$scope','$window', '$location', '$cookieStore','ordenTrabajoservice','usuarioservice','brockerservice', function($scope, $window, $location, $cookieStore, ordenTrabajoservice,usuarioservice,brockerservice){
+	ordenTrabajoservice.loadOT($cookieStore.get("idOt")).then(function(data){
+		$scope.ot= data;
+		ordenTrabajoservice.loadCliente($scope.ot.idCliente).then(function(data) {
+			$scope.cliente = data;
+			console.log("Datos de Cliente",data);
+		});
+	});	
+}]);	
+
+
+
